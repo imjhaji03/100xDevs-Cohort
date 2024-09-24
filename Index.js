fefe03fc45a755f35bcbd560a79fb78f02ea32a7
imjhaji03 <jamit5492@gmail.comm>
2024-09-24 22:52:33 +0530

    commit

--- Index.js
+++ Index.js
@@ -1,5 +1,12 @@
-let name = "john";
-const age = 30;
-var isStudent = false;
+// VARIABLES
+// Execution happens from 1st line to last line
+let firstName = "john"; //can be reassigned
+const age = 30; //const var that cannot be reassigned
+var isStudent = false; //older way to declare variable ,function scoped
 
-console.log(name)
+console.log(firstName)
+console.log("firstName") //""prints the actual value inside ""
+console.log(age)
+console.log(isStudent) //Value is what gets printed over here..
+
+
