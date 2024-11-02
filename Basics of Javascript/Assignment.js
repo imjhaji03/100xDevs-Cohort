970553d975be0badbea718810a61f7dd97d75214
Amit Gunjan Jha <jamit5492@gmail.com>
2024-11-02 12:43:32 +0530

    commit

---
+++ Basics of Javascript/Assignment.js
@@ -0,0 +1,18 @@
+// Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
+
+let FavouriteColour = "Black";
+const Height = 165;
+var LikePizza = true;
+
+console.log(FavouriteColour)
+console.log(Height)
+console.log(LikePizza)
+
+// Write a function sum that finds the sum of two numbers.Side quest - Try passing in a string instead of a number and see what happens?
+
+function sum (a, b ){
+    return = a + b;
+}
+
+let Sum = sum(1, 2);
+console.log(Sum)
